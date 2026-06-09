5a128b7ea40ea9c31989cb24ced0c789c56d706c
sabirmaroua12-rgb <sabirmaroua12@gmail.com>
2026-06-09 17:22:16 +0200

    create the structure of project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from "expo-router";
+import { HeaderShownContext } from '@react-navigation/elements';
+
 
 export default function RootLayout() {
-  return <Stack />;
+  <GestureHandlerRootView style={{}}
+  return <Stack  screenOptions={{headerShown:false}}/>;
 }
